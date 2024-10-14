4938bc85cc085008efdb99e95c67f6115274fb46
Jayasree <jspa9999@gmail.com>
2024-10-14 14:04:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("code is executing through java script engine");
+
+for(let i=1;i<=100;i++){
+    console.log(i);
+
+}
+
+let myHeading = document.createElement("h1");
+myHeading.innerHTML = "This.HTML";
+document.body.append(myHeading);
